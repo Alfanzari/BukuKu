5bcf814b0f0f284fc9ad5ecc105aa641cbace040
Syauqi Al Fanzari <alfanzarisyauqi@gmail.com>
2026-06-13 08:22:14 -0700

    Tambah BukuApiService untuk koneksi ke API

--- app/src/main/java/com/syauqialfanzari0008/bukuku/network/BukuApiService.kt
+++ app/src/main/java/com/syauqialfanzari0008/bukuku/network/BukuApiService.kt
@@ -1,2 +1,62 @@
 package com.syauqialfanzari0008.bukuku.network
 
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import com.syauqialfanzari0008.bukuku.model.Buku
+import com.syauqialfanzari0008.bukuku.model.OpStatus
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.DELETE
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.Multipart
+import retrofit2.http.POST
+import retrofit2.http.Part
+import retrofit2.http.Query
+
+private const val BASE_URL = "https://gh.d3ifcool.org/"
+
+private val moshi = Moshi.Builder()
+    .add(KotlinJsonAdapterFactory())
+    .build()
+
+private val retrofit = Retrofit.Builder()
+    .addConverterFactory(MoshiConverterFactory.create(moshi))
+    .baseUrl(BASE_URL)
+    .build()
+
+interface BukuApiService {
+    @GET("buku.php")
+    suspend fun getBuku(
+        @Header("Authorization") userId: String
+    ): List<Buku>
+
+    @Multipart
+    @POST("buku.php")
+    suspend fun postBuku(
+        @Header("Authorization") userId: String,
+        @Part("judul") judul: RequestBody,
+        @Part("penulis") penulis: RequestBody,
+        @Part image: MultipartBody.Part
+    ): OpStatus
+
+    @DELETE("buku.php")
+    suspend fun deleteBuku(
+        @Header("Authorization") userId: String,
+        @Query("id") id: String
+    ): OpStatus
+}
+
+object BukuApi {
+    val service: BukuApiService by lazy {
+        retrofit.create(BukuApiService::class.java)
+    }
+
+    fun getBukuUrl(imageId: String): String {
+        return "${BASE_URL}image.php?id=$imageId"
+    }
+}
+
+enum class ApiStatus { LOADING, SUCCESS, FAILED }
